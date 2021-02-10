0199ad0d82db3bcceff9873770532f7ff02e2234
dscaba <68200321+dscaba@users.noreply.github.com>
2021-02-10 12:35:49 -0300

    Update Pytube.py
    Se agregó una barra de progreso de descarga

--- Pytube.py
+++ Pytube.py
@@ -1,16 +1,35 @@
 import os
 import time
-from pytube import YouTube
-from art import *
+import sys
+import re
 
+try:
+    from pytube import YouTube
+    from pytube import request
+    from pytube.cli import on_progress
+    from art import *
+except ImportError:
+    import subprocess
+    subprocess.run("pip install pytube art")
+    from pytube import YouTube
+    from pytube import request
+    from pytube.cli import on_progress
+    from art import *
+    
 fig = text2art("YouTube Downloader")
 print(fig)
 
-link = input("Ingrese el link del video a descargar: ")
-yt = YouTube(link)
+url = input("Ingrese el link del video a descargar: ")
+
+
+yt = YouTube(url, on_progress_callback=on_progress)
+
+
 user = os.getlogin()
 
 location = "C:\\Users\\" + user + "\\Desktop"
+
+
 
 
 print("¿Qué desea descargar?"'\n')
@@ -20,16 +39,16 @@ choice = int(input("Elija la opción deseada: "))
 while True:
     if choice == 1:
         print("Usted Descargará el siguiente video: ", yt.title)
-        ys = yt.streams.get_highest_resolution()
+        yt.streams.get_highest_resolution().download(location)
         print("La descaga ha comenzado..."'\n')
-        ys.download(location)
+
         print("Descarga completada ✔")
         break
     elif choice == 2:
         print("Usted Descargará el audio siguiente video: ", yt.title)
-        ys = yt.streams.get_audio_only(subtype="mp4")
+        yt.streams.get_audio_only(subtype="mp4").download(location)
         print("La descaga ha comenzado..."'\n')
-        ys.download(location)
+    
         print("Descarga realizada con éxito!")
         break
 
